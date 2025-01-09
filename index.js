123e0f41028d3129909400ea3b88d160220287cd
Matěj Pavlíček <tikinang@gmail.com>
2025-01-09 11:03:48 +0100

    wau

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('.'));
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
--- main.go
+++ main.go
@@ -3,12 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
-
-	"go.elastic.co/apm/v2"
 )
 
 func main() {
-	fmt.Println(apm.AgentVersion)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		_, _ = fmt.Fprintln(w, "OK")
